7cc9320c56aff9c4c550de2af949e9240d939ce8
Barbs <barbarap095@gmail.com>
2020-07-31 14:21:31 +1000

    Add employee and department prompts

--- employee.js
+++ employee.js
@@ -3,33 +3,33 @@ const inquirer = require("inquirer");
 const conTable = require("console.table");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  port: 3306,
+    port: 3306,
 
-  user: "root",
+    user: "root",
 
-  password: "Snoopy123!",
-  database: "employee_DB"
+    password: "Snoopy123!",
+    database: "employee_DB"
 });
 
 // Establish connection
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     startingPrompt();
-  });
+});
 
 // starting prompt
 function startingPrompt() {
-    inquirer.prompt ({
+    inquirer.prompt({
         name: "add",
         type: "list",
         message: "Welcome to Employee Tracker. What would you like to do?",
         choices: [
-            "Add employee", 
+            "Add employee",
             "Add role",
-            "Add department", 
+            "Add department",
             "View all employees",
             "View roles",
             "View departments",
@@ -37,46 +37,49 @@ function startingPrompt() {
             "Finished"
         ]
 
-    }).then(function(response) {
-        switch(response) {
+    }).then(function (response) {
+        switch (response) {
             case "Add employee":
                 addEmployee();
-            break;
+                break;
 
             case "Add role":
                 addRole();
-            break;
+                break;
 
             case "Add department":
                 addDept();
-            break;
+                break;
 
             case "View all employees":
                 viewEmployees();
-            break;
+                break;
 
             case "View roles":
                 viewRoles();
-            break;
+                break;
 
             case "View departments":
                 viewDepts();
-            break;
+                break;
 
             case "Update Employee roles":
                 updateRoles();
-            break;
+                break;
 
             case "Finished":
                 console.log("You are all done!");
                 connection.end();
-            break;
-            
+                break;
+
             default:
                 startingPrompt();
-            
+
         }
     })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
 function addEmployee() {
@@ -102,42 +105,90 @@ function addEmployee() {
             message: "Who is the employee's manager?"
         },
 
-    ]);
+    ]).then(function (response) {
+        connection.query("INSERT INTO employee SET ? ",
+            {
+                first_name: response.first_name,
+                last_name: response.last_name,
+            }
+
+        )
+    })
 };
 
 function addRole() {
-    inquirer.prompt([{
+    inquirer.prompt([
+        {
+            name: "title",
+            type: "input",
+            message: "What is the title of your role in the company"
 
-    }])
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this role?"
+
+        },
+
+    ]).then(function (response) {
+        connection.query("INSERT INTO employee SET ? ",
+            {
+                title: response.title,
+                salary: response.salary,
+            }
+
+        )
+    })
 }
 
 function addDept() {
-    inquirer.prompt([{
+    inquirer.prompt([
+        {
+            name: "deptName",
+            type: "input",
+            message: "What is the name of your department?"
 
-    }])
+        },
+        {
+            name: "salary",
+            type: "input",
+            message: "What is the salary for this role?"
+
+        },
+
+    ]).then(function (response) {
+        connection.query("INSERT INTO employee SET ? ",
+            {
+                title: response.title,
+                salary: response.salary,
+            }
+
+        )
+    })
 }
 
 function viewEmployees() {
-    inquirer.prompt([{
+            inquirer.prompt([{
 
-    }])
-}
+            }])
+        }
 
 function viewRoles() {
-    inquirer.prompt([{
+            inquirer.prompt([{
 
-    }])
-}
+            }])
+        }
 
 function viewDepts() {
-    inquirer.prompt([{
+            inquirer.prompt([{
 
-    }])
-}
+            }])
+        }
 
 function updateRoles() {
-    inquirer.prompt([{
+            inquirer.prompt([{
 
-    }])
-}
+            }])
+        }
 
---
+++ seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (id, name);
+
+INSERT INTO role (title, salary, department_id) 
+VALUES 
+
+INSERT INTO employee (first_name, last_name, role, manager);
